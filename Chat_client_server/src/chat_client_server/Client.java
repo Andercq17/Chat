3cb6a399ab2f93bf60a9b4656f39883f33e2554b
Andersson <a.cardenas.quiros@gmail.com>
2023-04-02 12:30:18 -0500

    Reestructuración de codigo en clase client

--- Chat_client_server/src/chat_client_server/Client.java
+++ Chat_client_server/src/chat_client_server/Client.java
@@ -2,21 +2,54 @@
 package chat_client_server;
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
 public class Client {
+    private Socket socket;
+    private BufferedReader in;
+    private PrintWriter out;
     public static void main(String[] args) throws IOException{
-        String hostName = "localhost";
-        int portNumber = 1234;
+        Scanner scanner = new Scanner(System.in);
 
-        Socket socket = new Socket(hostName, portNumber);
+        System.out.print("Enter server IP: ");
+        String serverIp = scanner.nextLine();
+
+        Socket socket = new Socket(serverIp, 1234);
+        System.out.println("Connected to server");
+
         BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 
-        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-        String userInput;
-        while ((userInput = stdIn.readLine()) != null) {
-            out.println(userInput);
-            System.out.println("Server said: " + in.readLine());
+        String username;
+        do {
+            System.out.print("Enter your username: ");
+            username = scanner.nextLine();
+            out.println(username);
+        } while (in.readLine().startsWith("Username taken"));
+
+        System.out.println("Welcome, " + username + "!");
+
+        Thread inputThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String inputLine;
+                    while ((inputLine = scanner.nextLine()) != null) {
+                        out.println(inputLine);
+                    }
+                } catch (Exception e) {
+                    System.out.println("Error reading input from user");
+                }
+            }
+        });
+        inputThread.start();
+
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            System.out.println(inputLine);
         }
+
+        socket.close();
+        scanner.close();
     }
 }
