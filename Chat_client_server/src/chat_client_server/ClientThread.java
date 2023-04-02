5ee318d4bcf863737e42b8eded21a43211928374
Andersson <a.cardenas.quiros@gmail.com>
2023-04-02 12:25:58 -0500

    Reestructuración de codigo para comunicación privada entre clientes

--- Chat_client_server/src/chat_client_server/ClientThread.java
+++ Chat_client_server/src/chat_client_server/ClientThread.java
@@ -6,6 +6,7 @@ public class ClientThread implements Runnable {
     private Socket clientSocket;
     private BufferedReader in;
     private PrintWriter out;
+    private String username;
 
     public ClientThread(Socket socket) throws IOException {
         this.clientSocket = socket;
@@ -14,10 +15,25 @@ public class ClientThread implements Runnable {
     }
     public void run(){
         try {
+            out.println("Enter your username:");
+            this.username = in.readLine();
+            out.println("Welcome, " + this.username + "!");
+
             String inputLine;
             while ((inputLine = in.readLine()) != null) {
                 System.out.println("Received message: " + inputLine);
-                out.println("You said: " + inputLine);
+                if (inputLine.startsWith("/private")) {
+                    String[] parts = inputLine.split(" ", 3);
+                    if (parts.length == 3) {
+                        String recipientName = parts[1];
+                        String message = parts[2];
+                        Server.sendPrivateMessage(message, this, recipientName);
+                    } else {
+                        sendMessage("Usage: /private <username> <message>");
+                    }
+                } else {
+                    Server.broadcast(inputLine, this);
+                }
             }
         } catch (IOException e) {
             System.out.println("Error handling client");
@@ -29,5 +45,12 @@ public class ClientThread implements Runnable {
             }
             System.out.println("Client disconnected");
         }
+    }
+    public void sendMessage(String message) {
+        out.println(message);
+    }
+
+    public String getUsername() {
+        return this.username;
     }
 }
--- Chat_client_server/src/chat_client_server/Server.java
+++ Chat_client_server/src/chat_client_server/Server.java
@@ -5,6 +5,7 @@ import java.net.*;
 import java.util.*;
 
 public class Server {
+    private static List<ClientThread> clients = new ArrayList<>();
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(1234);
         System.out.println("Server started");
@@ -13,9 +14,29 @@ public class Server {
             Socket clientSocket = serverSocket.accept();
             System.out.println("Client connected");
 
-            Thread thread = new Thread(new ClientThread(clientSocket));
+            ClientThread client = new ClientThread(clientSocket);
+            clients.add(client);
+
+            Thread thread = new Thread(client);
             thread.start();
+        }    
+    }
+     public static void sendPrivateMessage(String message, ClientThread sender, String recipientName) {
+        for (ClientThread client : clients) {
+            if (client.getUsername().equals(recipientName)) {
+                client.sendMessage(sender.getUsername() + " (private): " + message);
+                sender.sendMessage("You (private) -> " + recipientName + ": " + message);
+                return;
+            }
         }
-        
-   }
+        sender.sendMessage("User " + recipientName + " not found.");
+    }
+
+    public static void broadcast(String message, ClientThread sender) {
+        for (ClientThread client : clients) {
+            if (client != sender) {
+                client.sendMessage(sender.getUsername() + ": " + message);
+            }
+        }
+    }
 }
